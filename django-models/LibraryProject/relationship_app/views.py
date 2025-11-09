6926a88c75a6a2dd842f06eb25543e0539c8e668
Nouh <noohahmed600@gmail.com>
2025-11-09 13:52:57 +0300

    added the DetailView module

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Library, Book, Author, Librarian
 
 # Create your views here.
