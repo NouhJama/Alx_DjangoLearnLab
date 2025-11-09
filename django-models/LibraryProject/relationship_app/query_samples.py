2314df115c32c42895a34ebd2057ba945d778684
Nouh <noohahmed600@gmail.com>
2025-11-09 12:00:37 +0300

    modified the query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ def get_books_by_author(author_name):
 
 # List all books in the library
 def get_books_in_library(library_name):
-    return Book.objects.filter(libraries__name=library_name)
+    return Book.objects.filter(libraries__name=library_name), Book.objects.all()
 
 # Retrieve the librarian of a specific library
 def get_librarian_of_library(library_name):
