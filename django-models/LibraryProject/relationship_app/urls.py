615882b0e3736df651372c0348ddd2f76e6a10c8
Nouh <noohahmed600@gmail.com>
2025-11-09 13:44:58 +0300

    added function-based and class-based view, updated the url configurations and created templated for the different views

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Function-based view
+    path('books/function/', views.list_books, name='list-books-function'),
+    
+    # Class-based ListView patterns
+    path('books/', views.BookListView.as_view(), name='book-list'),
+    path('authors/', views.AuthorListView.as_view(), name='author-list'),
+    path('libraries/', views.LibraryListView.as_view(), name='library-list'),
+    
+    # Class-based DetailView patterns
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView, DetailView
+from relationship_app.models import Book, Author, Library, Librarian
 
 # Create your views here.
+
+# Function-based view (your original)
+def list_books(request):
+    books = Book.objects.all()
+    for book in books:
+        return f"Book: {book.title}, Author: {book.author.name}"
+
+# Class-based view for displaying a specific library details, listing all its books and librarian
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        context['books'] = library.books.all()
+        context['librarian'] = library.librarian
+        return context
+    
+
