b8a31445262b91e05bf4960539814d6987540177
Nouh <noohahmed600@gmail.com>
2025-11-12 13:14:15 +0300

    added cusom uer to the model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,16 +1,60 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
+from django.conf import settings
 
-# Custom User Model
+# This is an extention of the default User model.
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    # Additional fields added
+    date_of_birth = models.DateField(null=True, blank=True) 
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)   
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
+    # A nested Meta class to define permissions.
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_delete_book", "Can delete book"),
+            ("can_change_book", "Can change book"),
+        ]
+    
+class Library(models.Model):
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
+
+# Create UserProfile model to extend User model if needed
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # role: CharField with choices for 'Admin', 'Librarian', and 'Member'.
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return self.user.username
+    
