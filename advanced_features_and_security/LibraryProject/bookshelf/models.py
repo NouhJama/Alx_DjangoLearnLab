8b8c44a3c58671a6536380538dc47897bf21f925
Nouh <noohahmed600@gmail.com>
2025-11-12 13:16:53 +0300

    added CustomUserManager class

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.models import AbstractUser, User, BaseUserManager
 from django.conf import settings
 
 # This is an extention of the default User model.
@@ -58,3 +58,18 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
     
+class CustomeUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
