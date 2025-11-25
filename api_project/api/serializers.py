d5a05171a79fa12c9142877414f2d14ee9f03935
Nouh <noohahmed600@gmail.com>
2025-11-25 12:56:13 +0300

    Implement HyperlinkedModelSerializer and Author model

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Author
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
         fields = '__all__' # Serialize all fields of the Book model
+
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__' # Serialize all fields of the Author model       
