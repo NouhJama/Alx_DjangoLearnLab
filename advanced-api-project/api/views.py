b350c05f2783c6a1cbc2b843b0d28a83748afbfe
Nouh <noohahmed600@gmail.com>
2025-11-29 23:47:29 +0300

    added django_filter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Author, Book
 from .serializers import  BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
