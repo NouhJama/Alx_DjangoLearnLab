048dafcea2db0609c2587ce96f2a05704403c0e5
Nouh <noohahmed600@gmail.com>
2025-11-29 17:22:33 +0300

    added permission_classes to the view and imported necessary modules for them

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,50 +2,33 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import  BookSerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
-    def get_permissions(self):
-        if self.request.method in ['POST', 'PUT', 'DELETE', 'PATCH']:
-            return [permissions.IsAuthenticated()]
-        return [permissions.AllowAny()]
+    permission_classes = [IsAuthenticated]
     
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [permissions.AllowAny()]
-        return [permissions.IsAuthenticated()]
     
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
     
-    def get_permissions(self):
-        if self.request.method in ['PUT', 'DELETE']:
-            return [permissions.IsAuthenticated()]
-        return [permissions.AllowAny()]
-    
+
 class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
     
-    def get_permissions(self):
-        if self.request.method == 'DELETE':
-            return [permissions.IsAuthenticated()]
-        return [permissions.AllowAny()]
