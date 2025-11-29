4fd8cdabd79280b1dc9de1a5c723d64ac031dcb9
Nouh <noohahmed600@gmail.com>
2025-11-29 17:13:29 +0300

    modified the views and added new view for every CRUD action. then added the paths for each endpoint

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,36 +1,51 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import generics
 from .models import Author, Book
 from .serializers import  BookSerializer
 from rest_framework import permissions
 
 # Create your views here.
-# View for listing and creating Books
-class BookListCreateView(ListCreateAPIView):
+class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    
     def get_permissions(self):
-        if self.request.method == 'POST':
-            """
-            Only authenticated users can create a new Book
-            -GET (list) is open to all users
-            -POST (create) requires authentication
-            """
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+    
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'DELETE', 'PATCH']:
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
-
-# View for retrieving, updating, and deleting a Book
-class BookRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    
+class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    
     def get_permissions(self):
-        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
-            """
-            Only authenticated users can update or delete a Book
-            -GET (retrieve) is open to all users
-            -PUT, PATCH, DELETE require authentication
-            """
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+    
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+    
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'DELETE':
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
