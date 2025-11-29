c066c4912c5127fa9de0e877ff2b1290d53e1a80
Nouh <noohahmed600@gmail.com>
2025-11-30 00:01:43 +0300

    added all tree backends to the filter_backends

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
-from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework import filters
 from .models import Author, Book
 from .serializers import  BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
@@ -12,7 +12,7 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter] # Add filtering all three backends
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter] # Add filtering all three backends
     filterset_fields = ['author', 'title', 'publication_year'] # Fields to filter by
     search_fields = ['title', 'author__name']  # Fields to search
     ordering_fields = ['title', 'publication_year', 'author__name']  # Fields
