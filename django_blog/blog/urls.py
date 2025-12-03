b6303b846dbd605ec3ff1c9c9733ef8e9c5303b6
Nouh <noohahmed600@gmail.com>
2025-12-03 12:44:08 +0300

    added the profile path

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register', template_name='blog/registration.html'),
+    path('profile/', views.profile, name='profile', template_name='blog/profile.html'),
+    path('', views.home, name='blog-home'),
 ]
