84a5e03f26339deba3ae4320b9ea46e89bc9e146
Nouh <noohahmed600@gmail.com>
2025-12-04 12:56:54 +0300

    Created comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -18,3 +18,13 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+    
+class comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
