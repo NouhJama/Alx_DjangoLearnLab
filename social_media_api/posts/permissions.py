9b1c9b90a85e2de7f830ffe4b22109e5d335f735
Nouh <noohahmed600@gmail.com>
2025-12-12 17:17:54 +0300

    -Creat viewsets for the CRUD operations -Create custome persmissions for views.

---
+++ social_media_api/posts/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+# Custom permissions only logged-in users can create posts/comments
+class IsAuthenticatedOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return request.user and request.user.is_authenticated
+
+# Custom permission to allow only owner to edit or delete their objects(posts/comments)
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to the author of the post/comment.
+        return obj.author == request.user
+    
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Post, Comment
+from rest_framework import viewsets
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from posts.permissions import IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly
+
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)   
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
