6c4e32bb8ce223eea2e0efb4bfa78026dac2b639
Nouh <noohahmed600@gmail.com>
2025-12-12 17:02:05 +0300

    implemented serializers for the posts and comments models

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,4 +1,3 @@
-from rest_framework.authtoken.views import ObtainAuthToken
 from .views import UserRegistrationView, LoginView, PostViewSet, CommentViewSet, ProfileViewSet
 from django.urls import path, include
 from rest_framework import routers
@@ -11,10 +10,8 @@ router.register(r'profile', ProfileViewSet, basename='profile')
 
 
 urlpatterns = [
-    path("login/", LoginView.as_view(), name="api_token_auth"),
+    path("login/", LoginView.as_view(), name="login"),
     path("register/", UserRegistrationView.as_view(), name="user_registration"),
-    path("auth/api/", ObtainAuthToken.as_view(), name="api_token_auth"),
-
 ]
 
 urlpatterns += router.urls
