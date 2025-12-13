d03354f08b700ab33717925d2b5731800047a032
Nouh <noohahmed600@gmail.com>
2025-12-13 16:35:58 +0300

    Add timestamp field to the notifications model ins the notifications app.

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -7,7 +7,7 @@ class Notification(models.Model):
     actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_notifications')
     verb = models.TextField()# type of action e.g., 'liked', 'commented'
     target = models.ForeignKey('posts.Post', on_delete=models.CASCADE, blank=True,)
-    created_at = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'Notification to {self.recipient.username} from {self.actor.username} at {self.created_at}'
