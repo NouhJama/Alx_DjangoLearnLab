be618136e86ac6435683da1a2ce87ee4b9525af7
Nouh <noohahmed600@gmail.com>
2025-12-13 16:46:30 +0300

    Include Like post urls pattern in the router

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, LikePostViewSet
 from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
+router.register(r'like', LikePostViewSet, basename='likepost')
 
 urlpatterns = [
     path("", include(router.urls)),
