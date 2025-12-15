5ebe0ecc51b5ee299e350b917db475d663d4cb2c
Nouh <noohahmed600@gmail.com>
2025-12-15 14:38:57 +0300

    Add follow and unfollow urls'

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView
+from .views import PostViewSet, CommentViewSet, LikePostView, UnlikePostView, FollowView, UnfollowView
 from django.urls import path, include
 
 router = routers.DefaultRouter()
@@ -10,4 +10,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('follow/<int:pk>/', FollowView.as_view(), name='follow-user'),
+    path('unfollow/<int:pk>/', UnfollowView.as_view(), name='unfollow-user'),
 ]
